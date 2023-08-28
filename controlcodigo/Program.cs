e937ff9a018f3a893f7204dd7665f4ba22aec03a
axelrodrig <140225196+axelrodrig@users.noreply.github.com>
2023-08-28 16:42:28 -0600

    un solo mensaje

--- controlcodigo/Program.cs
+++ controlcodigo/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("holaaaa,axel rodriguez");
-Console.WriteLine("holaaaa,axel rodriguez");
-Console.WriteLine("holaaaa,axel rodriguez");
-Console.WriteLine("holaaaa,axel rodriguez");
+

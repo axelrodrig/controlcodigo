bd07ba300fcbcaa6932be83b9e10f9b1a33a2cc8
Axel Rodríguez <axel07430@gmail.com>
2023-08-28 16:44:29 -0600

    muchos mensajes

--- controlcodigo/Program.cs
+++ controlcodigo/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+Console.WriteLine("holaaaa,axel rodriguez");
+Console.WriteLine("holaaaa,axel rodriguez");
+Console.WriteLine("holaaaa,axel rodriguez");
+Console.WriteLine("holaaaa,axel rodriguez");
